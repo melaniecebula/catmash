3f4fe2e1caef6c6e17a6a2f7ead0aebb9ef8f1f6
Chyheim <cajackso@ucsd.edu>
2013-07-11 21:18:43 -0700

    basic implementation of LGCatStore

---
+++ Catmash/Catmash/LGCatStore.h
@@ -0,0 +1,15 @@
+//
+//  LGCatStore.h
+//  Catmash
+//
+//  Created by Chyheim Alon Jackson-Burgess on 7/11/13.
+//  Copyright (c) 2013 TheLGs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "LGCat.h"
+@interface LGCatStore : NSObject
+@property (nonatomic, strong, readonly) NSArray *catStore;
++(LGCatStore *)sharedStore;
+-(void)addToCatStore:(LGCat *)cat;
+@end
---
+++ Catmash/Catmash/LGCatStore.m
@@ -0,0 +1,39 @@
+//
+//  LGCatStore.m
+//  Catmash
+//
+//  Created by Chyheim Alon Jackson-Burgess on 7/11/13.
+//  Copyright (c) 2013 TheLGs. All rights reserved.
+//
+
+#import "LGCatStore.h"
+@interface LGCatStore()
+{
+    NSMutableArray *_catStore;
+}
+@end
+@implementation LGCatStore
++(id)allocWithZone:(NSZone *)zone{
+    return [self sharedStore];
+}
+- (id)init{
+    self = [super init];
+    
+    if(self){
+        _catStore = [[NSMutableArray alloc] init];
+    }
+    
+    return self;
+}
++(LGCatStore *)sharedStore{
+    static LGCatStore *sharedStore = nil;
+    if(!sharedStore){
+        sharedStore = [[super allocWithZone:nil] init];
+    }
+    
+    return sharedStore;
+}
+-(void)addToCatStore:(LGCat *)cat{
+    [_catStore addObject:cat];
+}
+@end

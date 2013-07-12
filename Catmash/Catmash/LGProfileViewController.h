7a62f14e8aa16d9ecfd000be44131786e63507c6
honeyzee <hannia.zia@yale.edu>
2013-07-11 21:33:48 -0700

    added the profile view

---
+++ Catmash/Catmash/LGProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  LGProfileViewController.h
+//  Catmash
+//
+//  Created by Hannia Zia on 7/11/13.
+//  Copyright (c) 2013 TheLGs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LGProfileViewController : UIViewController
+
+@end

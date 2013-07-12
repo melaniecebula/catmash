fde64e36ef5dbe97d9923edb1bbc614b4fc2e885
Steven Beck <sbeck@sbeck-mbp.local>
2013-07-11 22:00:01 -0700

    Added Battle view

---
+++ Catmash/Catmash/LGBattleViewViewController.h
@@ -0,0 +1,13 @@
+//
+//  LGBattleViewViewController.h
+//  Catmash
+//
+//  Created by Steven Beck on 7/11/13.
+//  Copyright (c) 2013 TheLGs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LGBattleViewViewController : UIViewController
+
+@end

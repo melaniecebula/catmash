b8a468c0d8790ad20266fd152d4ac979d419118e
Melanie Cebula <melaniecebula@melaniecebu-mbp.local>
2013-07-11 20:31:56 -0700

    hey guys

---
+++ Catmash/LGCatBattleViewController.h
@@ -0,0 +1,13 @@
+//
+//  LGCatBattleViewController.h
+//  Catmash
+//
+//  Created by Melanie Cebula on 7/11/13.
+//  Copyright (c) 2013 TheLGs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LGCatBattleViewController : UIViewController
+
+@end
